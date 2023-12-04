4a74f95d3a06dfac8fbe40b207ba704e20e6e62f
ujvarivera <ujvariveronika0320@gmail.com>
2023-12-04 21:46:11 +0100

    buy coffee until you have enough money

---
+++ coffee_machine.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import time
+from random import random, randint
+import json
+
+THINGSBOARD_HOST = "demo.thingsboard.io"
+DEVICE_ACCESS_TOKEN = "7tZs7Otx4j5FIe9tqqmU"
+SLEEP_TIME = 3
+COFFEE_PRICE = 1000
+sensor_data = {'money_left': 2000, 'has_enough_money': True}
+    
+client = mqtt.Client()
+client.username_pw_set(DEVICE_ACCESS_TOKEN, "")
+client.connect(THINGSBOARD_HOST, 1883, 60)
+client.loop_start()
+client.publish("v1/devices/me/telemetry", json.dumps(sensor_data))
+print("Start money: 2000, and a coffee costs ", COFFEE_PRICE)
+
+try:
+    while True:
+        coffee_bought = int(input("Buy coffee (1: yes, 0: no): "))
+        if (coffee_bought == 1):
+            if (sensor_data["has_enough_money"] == True and sensor_data["money_left"] - COFFEE_PRICE >= 0): 
+                sensor_data["money_left"] = sensor_data["money_left"] - COFFEE_PRICE
+                if(sensor_data["money_left"] == 0): sensor_data["has_enough_money"] = False
+                client.publish("v1/devices/me/telemetry", json.dumps(sensor_data))
+                print("Coffee bought ", sensor_data)
+                time.sleep(SLEEP_TIME)
+            else: 
+                print("Not enough money")
+                sensor_data["has_enough_money"] = False
+                client.publish("v1/devices/me/telemetry", json.dumps(sensor_data))
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect()
